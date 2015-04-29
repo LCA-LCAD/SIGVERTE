901df3dca7987d921bd83e1265992b74986ca2fd
franktominc <frankptominc@gmail.com>
2015-04-29 15:46:14 -0300

    Removed unused statements

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -21,14 +21,5 @@ public class Application extends Controller {
         return ok(index.render());
     }
 
-    public static Result addPerson() {
-    	Person person = Form.form(Person.class).bindFromRequest().get();
-    	person.save();
-    	return redirect(routes.Application.index());
-    }
 
-    public static Result getPersons() {
-    	List<Person> persons = new Model.Finder(String.class, Person.class).all();
-    	return ok(toJson(persons));
-    }
 }
