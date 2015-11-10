9dd9a79323361af215a003d75c1565be26aa8430
lcad <rodrigosenger@outlook.com>
2015-11-10 18:17:08 -0200

    Routes error

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,29 +10,59 @@ import play.mvc.Http;
 import play.mvc.Result;
 import views.html.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import static play.data.Form.form;
+
 
 public class Application extends Controller {
 
-    public static Result upload(){
+    public static Result upload() {
 
-        Http.MultipartFormData body = request().body().asMultipartFormData();
-        Http.MultipartFormData.FilePart picture = body.getFile("picture");
-        if(picture !=null){
-            String calhaId = Form.form(Calha.class).bindFromRequest().get().getId().toString();
-            String imagem = calhaId + ".png";
+        MultipartFormData body = request().body().asMultipartFormData();
+        FilePart picture = body.getFile("picture");
+        String extensaoPadraoDeImagens = Play.application().configuration().getString("extensaoPadraoDeImagens");
+        if (picture != null) {
+
+            String filmeId = form().bindFromRequest().get("calhaId");
+            String imagem = filmeId + extensaoPadraoDeImagens;
+            String contentType = picture.getContentType();
             File file = picture.getFile();
 
             String diretorioDeImagens = Play.application().configuration().getString("diretorioDeImagens");
-            file.renameTo(new File(diretorioDeImagens,imagem));
-            return ok(views.html.upload.render("Arquivo  \"" + imagem + "\" foi carregado com sucesso!"));
-        }else{
-            flash("error", "Erro ao fazer upload");
+            String contentTypePadraoDeImagens = Play.application().configuration().getString("contentTypePadraoDeImagens");
+
+            if (contentType.equals(contentTypePadraoDeImagens)) {
+
+                file.renameTo(new File(diretorioDeImagens,imagem));
+                return ok(views.html.upload.render("Arquivo  \"" + imagem + "\" do tipo [" + contentType + "] foi carregado com sucesso !"));
+
+            } else {
+
+                return ok(views.html.upload.render("Imagens apenas no formato \"" + contentTypePadraoDeImagens + "\" serão aceitas!"));
+
+            }
+
+        } else {
+            flash("error","Erro ao fazer upload");
             return redirect(routes.Application.index());
         }
+    }
+
+    public static Result imagem(String id) throws IOException {
+
+        String diretorioDeImagens = Play.application().configuration().getString("diretorioDeImagens");
+
+        File imagem = new File(diretorioDeImagens,id + ".png");
+
+        return ok(new FileInputStream(imagem));
+
     }
 
     public static Result formImage() {
